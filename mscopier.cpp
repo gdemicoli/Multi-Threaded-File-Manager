54152bc6edb95347fa4415118d2aafa4b94434f3
Gustavo <s3898300@student.rmit.edu.au>
2025-08-27 14:48:54 +1000

    Included error checks for input

--- mscopier.cpp
+++ mscopier.cpp
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <iostream>
 #include <pthread.h>
-// #include "common_threads.h"
 #include "zem.h"
 
+// FIX-ME: must check return type of all api functions
 const int MAX = 1;
 Zem empty(MAX);
 Zem full(0);
@@ -59,6 +57,52 @@ void *producer(void *arg)
 
 int main(int argc, char *argv[])
 {
+    // fix-me:  check if source file exists...
+    if (argc != 4)
+    {
+        std::cerr << "Error: requires three arguements\n";
+        return 1;
+    }
+    std::string numThreadsStr = argv[1];
+    int threads = 0;
+
+    try
+    {
+        threads = std::stoi(numThreadsStr);
+        if (threads < 2 || threads > 10)
+        {
+            std::cerr << "Error: number of files must be between 2 & 10\n";
+            return 1;
+        }
+    }
+    catch (const std::invalid_argument &)
+    {
+        std::cerr << "Error: first arguement must be a valid integer\n";
+        return 1;
+    }
+    catch (const std::out_of_range &)
+    {
+        std::cerr << "Error: integer out of range\n";
+        return 1;
+    }
+
+    char *sourcefile = argv[2]; // char arrays
+    char *destFile = argv[3];   // char arrays
+
+    std::string source = "source";
+    std::string txt = ".txt";
+    pthread_t producerThreads[threads];
+    pthread_t consumerThreads[threads];
+
+    for (int i = 0; i < threads; i++)
+    {
+        std::string name = source + std::to_string(i + 1) + txt;
+        // *file is created on the heap so it lives no past the loop iteration
+        // fileData *file = new fileData(name, sourceDir, destDir); // char arrays are implcitly changed to std::string
+        pthread_create(&producerThreads[i], nullptr, producer, sourcefile);
+        pthread_create(&consumerThreads[i], nullptr, consumer, destFile);
+    }
+
     pthread_t producerThread;
     pthread_t consumerThread;
     pthread_create(&producerThread, nullptr, producer, nullptr);
